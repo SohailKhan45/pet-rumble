78c1fa1ccae03a3abd9b3796bd439cb809a4226b
SohailKhan <mdsohailkhan2267@gmail.com>
2024-07-30 14:07:24 +0530

    Modified .gitignore

---
+++ client/build/static/js/149.bf38f10e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[149],{7149:(t,c,a)=>{a.d(c,{kn0:()=>e});var h=a(6688);function e(t){return(0,h.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"}}]})(t)}}}]);
+//# sourceMappingURL=149.bf38f10e.chunk.js.map
---
+++ client/build/static/js/221.e6511f1e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[221],{221:(t,e,a)=>{a.d(e,{FyX:()=>n});var c=a(6688);function n(t){return(0,c.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M400 145.49L366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49z"}}]})(t)}}}]);
+//# sourceMappingURL=221.e6511f1e.chunk.js.map
